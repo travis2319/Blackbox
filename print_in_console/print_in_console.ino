8f9a134b052d7e734c53c79390f4246c550bcfad
travis fernandes <travisfernandes2327@gmail.com>
2024-01-25 15:53:01 +0530

    deleted some content

--- print_in_console/print_in_console.ino
+++ print_in_console/print_in_console.ino
@@ -5,9 +5,6 @@ SoftwareSerial GPS_SoftSerial(4, 5);/* (Rx, Tx) */
 /* Create an object named gps of the class TinyGPSPlus */
 TinyGPSPlus gps;              
 
-volatile float minutes, seconds;
-volatile int degree, secs, mins;
-
 void setup() {
  Serial.begin(9600);   /* Define baud rate for serial communication */
  GPS_SoftSerial.begin(9600); /* Define baud rate for software serial communication */
@@ -36,25 +33,12 @@ void loop() {
          Serial.println("*****");
         }
         else
-        {
-         DegMinSec(lat_val);
-         Serial.print("Latitude : ");
+        { 
+         Serial.print("Latitude : "); /* printing Latitude*/
          Serial.println(lat_val, 6);
-        //  Serial.print("Latitude in Degrees Minutes Seconds : ");
-        //  Serial.print(degree);
-        //  Serial.print("\t");
-        //  Serial.print(mins);
-        //  Serial.print("\t");
-        //  Serial.println(secs);
-         DegMinSec(lng_val); /* Convert the decimal degree value into degrees minutes seconds form */
-         Serial.print("Longitude : ");
+          
+         Serial.print("Longitude : "); /* printing Longitude*/
          Serial.println(lng_val, 6);
-        //  Serial.print("Longitude in Degrees Minutes Seconds : ");
-        //  Serial.print(degree);
-        //  Serial.print("\t");
-        //  Serial.print(mins);
-        //  Serial.print("\t");
-        //  Serial.println(secs);
         }
         
         if (!alt_valid)
@@ -97,16 +81,4 @@ static void smartDelay(unsigned long ms)
      gps.encode(GPS_SoftSerial.read());
 /* Encode basically is used to parse the string received by the GPS and to store it in a buffer so that information can be extracted from it */
   } while (millis() - start < ms);
-}
-
-void DegMinSec( double tot_val)           /* Convert data in decimal degrees into degrees minutes seconds form */
-{ 
-  degree = (int)tot_val;
-  minutes = tot_val - degree;
-  seconds = 60 * minutes;
-  minutes = (int)seconds;
-  mins = (int)minutes;
-  seconds = seconds - minutes;
-  seconds = 60 * seconds;
-  secs = (int)seconds;
 }
