c0a0ab9ecc0b1fe88f4cf2acfb7075273aa0c3df
travis fernandes <travisfernandes2327@gmail.com>
2024-03-29 08:13:50 +0530

    test_conn

---
+++ python_sript/test_conn.py
@@ -0,0 +1,26 @@
+import obd
+
+connection = obd.OBD("/dev/ttyACM0") # create connection with USB 0
+
+print(connection)
+
+cmd = obd.commands.SPEED
+
+response = connection.query(cmd)
+
+print(response.value)
+
+print(response.value.to("mph"))
+
+
+# ports = obd.scan_serial()      # return list of valid USB or RF ports
+# print ports                    # ['/dev/ttyUSB0', '/dev/ttyUSB1']
+# connection = obd.OBD(ports[0]) # connect to the first port in the list
+
+
+
+# connection = obd.OBD() # auto connect
+
+# # OR
+
+# OR
